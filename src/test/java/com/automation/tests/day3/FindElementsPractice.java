07d1b930d2f5ee35faa1b77c5f65bd60c5616ecc
erfan1336 <erfan1336@gmail.com>
2020-03-03 17:33:02 -0800

    web elements updated commit

--- src/test/java/com/automation/tests/day3/FindElementsPractice.java
+++ src/test/java/com/automation/tests/day3/FindElementsPractice.java
@@ -2,22 +2,48 @@ package com.automation.tests.day3;
 
 import com.automation.tests.utilities.DriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FindElementsPractice {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 //        WebDriverManager.chromedriver().setup();
 //        WebDriver driver = new ChromeDriver();
 
         WebDriver driver = DriverFactory.CreateADriver("chrome");
         driver.get("http://practice.cybertekschool.com/sign_up");
+        WebElement fullName = driver.findElement(By.name("full_name"));
+        fullName.sendKeys("Mister Twister");
 
+        Thread.sleep(3000);
 
+        WebElement email = driver.findElement(By.name("email"));
+        email.sendKeys("sdet@cybertekschoo.com");
 
+        Thread.sleep(3000);
 
+        WebElement signUp = driver.findElement(By.name("wooden_spoon"));
+        //when you see type="submit", you can use submit() instead of click()
+        //it make sense to use when click() method doesn't work
 
+        signUp.submit();
+
+        Thread.sleep(3000);
+
+        String expected = "Thank you for signing up. Click the button below to return to the home page.";
+
+        WebElement message = driver.findElement(By.className("subheader"));
+
+        String actual = message.getText();
+
+        if (expected.equals(actual)){
+            System.out.println("Test Has Passed.");
+        }else {
+            System.out.println("It Has Failed");
+        }
         driver.quit();//close everything
     }
 }
