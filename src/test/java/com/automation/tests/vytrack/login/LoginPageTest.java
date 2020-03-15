9ce4c75bf171ef46f273eb59700e1ea6e8a7d2fe
erfan1336 <erfan1336@gmail.com>
2020-03-15 09:35:20 -0700

    Day 9, vytrack automation

--- src/test/java/com/automation/tests/vytrack/login/LoginPageTest.java
+++ src/test/java/com/automation/tests/vytrack/login/LoginPageTest.java
@@ -1,10 +1,15 @@
 package com.automation.tests.vytrack.login;
 
+import com.automation.tests.utilities.BrowserUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 //STATIC IMPORT OF ALL ASSERTIONS
 import java.security.PublicKey;
@@ -20,6 +25,20 @@ public class LoginPageTest {
     //HTTPs - data encrypted, no chance for hackers to retrieve info
     //HTTP - data as a plain text, very easy to hack it
     private String URL = "http://qa2.vytrack.com/user/login";
+    private By usernameBy = By.id("prependedInput");
+    private By passwordBy = By.id("prependedInput2");
+    private By warningMessageBy = By.className("alert alert-error");
+
+
+    @Test(description = "verify that warning message displays when user enters invalid username")
+    public void invalidUsername(){
+        driver.findElement(usernameBy).sendKeys("invalidusername");
+        driver.findElement(passwordBy).sendKeys("UserUser123", Keys.ENTER);
+        BrowserUtils.wait(3);
+        WebElement warningElement = driver.findElement(warningMessageBy);
+        assertTrue(warningElement.isDisplayed());
+
+    }
 
     @BeforeMethod
     public void setup(){
