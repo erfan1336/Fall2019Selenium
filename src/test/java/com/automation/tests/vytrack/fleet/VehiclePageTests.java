6c3463f85612a0f325dc02c38ff770d118c58cff
erfan1336 <erfan1336@gmail.com>
2020-03-15 12:38:40 -0700

    Day 9, vytrack automation

--- src/test/java/com/automation/tests/vytrack/fleet/VehiclePageTests.java
+++ src/test/java/com/automation/tests/vytrack/fleet/VehiclePageTests.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -57,6 +58,10 @@ public class VehiclePageTests {
         //find subtitle Element
         WebElement subtitleElement = driver.findElement(subtitle);
         System.out.println(subtitleElement.getText());
+
+        String expected = "All cars";
+        String actual = subtitleElement.getText();
+        Assert.assertEquals(actual,expected);
     }
 
 
